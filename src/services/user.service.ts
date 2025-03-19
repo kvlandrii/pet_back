38bcb69957b42a883f4f17a61798af03b5216873
kvlandrii <andrijkoval30@gmail.com>
2025-03-19 15:47:13 +0200

    fixed register error

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,8 +2,12 @@ import bcrypt from 'bcrypt'
 import { User } from '../models/user.model'
 import jwt from 'jsonwebtoken'
 import { config } from '../config/env'
+import { Conflict } from 'http-errors'
 
 export const registerUser = async (name: string, email: string, password: string) => {
+    const user = await User.findOne({ email })
+    if (user) throw new Conflict('User already exists')
+
     const hashedPassword = await bcrypt.hash(password, 10)
     return await User.create({ name, email, password: hashedPassword })
 }
